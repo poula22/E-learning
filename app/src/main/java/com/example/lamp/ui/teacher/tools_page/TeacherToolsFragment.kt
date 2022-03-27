714db4759a6505d15379a466cb8e637bf2145daa
Pola Hano Shehat <43797542+polahano@users.noreply.github.com>
2022-03-27 03:34:26 +0200

    teacher tools databinding (xml missing)

--- app/src/main/java/com/example/lamp/ui/teacher/tools_page/TeacherToolsFragment.kt
+++ app/src/main/java/com/example/lamp/ui/teacher/tools_page/TeacherToolsFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lamp.R
+import com.example.lamp.databinding.FragmentTeacherToolsBinding
 import com.example.lamp.test_data.TestData
 import com.example.lamp.ui.teacher.tools_page.tools_recycler_view.TeacherToolsAdapter
 
 
 class TeacherToolsFragment:Fragment() {
+    lateinit var fragmentTeacherToolsBinding : FragmentTeacherToolsBinding
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: TeacherToolsAdapter
     override fun onCreateView(
@@ -19,8 +22,8 @@ class TeacherToolsFragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_teacher_tools,container,false)
+        fragmentTeacherToolsBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_teacher_tools,container,false)
+        return fragmentTeacherToolsBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
