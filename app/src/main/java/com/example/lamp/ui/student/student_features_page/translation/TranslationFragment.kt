559a3b7eaee43a165f76028aca5cd504f980ad75
Pola Hano Shehat <43797542+polahano@users.noreply.github.com>
2022-05-08 20:14:42 +0200

    Adding translation intents for images and documents(missing voice)

---
+++ app/src/main/java/com/example/lamp/ui/student/student_features_page/translation/TranslationFragment.kt
@@ -0,0 +1,79 @@
+package com.example.lamp.ui.student.student_features_page.translation
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.lamp.R
+import com.example.lamp.databinding.FragmentFeatureTranslationBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.github.dhaval2404.imagepicker.ImagePicker.Companion.REQUEST_CODE
+
+class TranslationFragment : Fragment() {
+
+    lateinit var viewBinding: FragmentFeatureTranslationBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_feature_translation, container, false
+        )
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        viewBinding.cardImage.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()                    //Crop image(Optional), Check Customization for more option
+//                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+//                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+
+
+//            var easyImage = EasyImage.Builder(this.requireContext())
+//            easyImage.allowMultiple(false)
+//                .setChooserTitle("Pick media")
+//                .setChooserType(ChooserType.CAMERA_AND_GALLERY)
+//                .setFolderName("EasyImage sample")
+//                .build();
+
+        }
+
+
+        viewBinding.cardDocument.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                val intentDocument = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    type = "*/*"
+                    putExtra(
+                        Intent.EXTRA_MIME_TYPES, arrayOf(
+                            "application/pdf",
+                            "application/doc",
+                            "application/docx",
+                            "text/plain"
+                        )
+                    )
+                }
+                startActivityForResult(intentDocument, REQUEST_CODE)
+            }
+        }
+
+
+        viewBinding.cardVoice.setOnClickListener {
+
+        }
+
+    }
+}
