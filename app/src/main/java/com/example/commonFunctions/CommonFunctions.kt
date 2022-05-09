0031d1160c75ae18a3573657eaac5d49785cdcf8
poula22 <poula.george@yahoo.com>
2022-05-09 03:27:10 +0200

    adding recitation views missing(recitation result,actions)

--- app/src/main/java/com/example/commonFunctions/CommonFunctions.kt
+++ app/src/main/java/com/example/commonFunctions/CommonFunctions.kt
@@ -1,64 +1,90 @@
 package com.example.commonFunctions
 
 import android.Manifest
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Build
+import android.os.Bundle
 import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
-import androidx.viewbinding.ViewBinding
+import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.example.lamp.R
+import com.github.dhaval2404.imagepicker.ImagePicker
 
 class CommonFunctions {
-//    companion object{
-//        lateinit var mediaRecorder: MediaRecorder
-//         fun  voiceRecord(viewBinding: ViewBinding){
-//
-//            var isRecording=false
-//            var colorRose=viewBinding.cardVoice.getContext().getResources().getColor(R.color.light_rose);
-//            var colorRed=viewBinding.cardVoice.getContext().getResources().getColor(R.color.red);
-//            if (isRecording){
-//                stopRecording()
-//                isRecording=false
-//
-//                viewBinding.cardVoice.setCardBackgroundColor(colorRose)
-//            }
-//            else{
-//                if (checkPermissions()){
-//                    startRecording()
-//                    viewBinding.cardVoice.setCardBackgroundColor(colorRed)
-//                }
-//                else{
-//                    var arr:Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
-//                    ActivityCompat.requestPermissions(requireActivity(),arr,21)
-//                }
-//                isRecording=true
-//            }
-//        }
-//
-//        private fun stopRecording() {
-//            mediaRecorder.stop()
-//            mediaRecorder.release()
-//
-//        }
-//
-//        private fun startRecording() {
-//            var recordPath:String?=requireActivity().getExternalFilesDir("/")?.absolutePath
-//            var recordFile="fileName.3gp"
-//            mediaRecorder= MediaRecorder()
-//            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-//            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-//            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-//            mediaRecorder.setOutputFile(recordPath+"/"+recordFile)
-//            mediaRecorder.prepare()
-//            mediaRecorder.start()
-//        }
-//
-//        @RequiresApi(Build.VERSION_CODES.M)
-//        fun checkPermissions():Boolean{
-//            return context?.checkSelfPermission(Manifest.permission.RECORD_AUDIO)== PackageManager.PERMISSION_GRANTED
-//        }
-//
-//    }
+    companion object{
+        fun imagePick(fragment:Fragment){
+            ImagePicker.with(fragment)
+                .crop()                    //Crop image(Optional), Check Customization for more option
+//                .compress(1024)			//Final image size will be less than 1 MB(Optional)
+//                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                .start()
+        }
+
+        fun uploadDoc(activity:FragmentActivity){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                val intentDocument = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    type = "*/*"
+                    putExtra(
+                        Intent.EXTRA_MIME_TYPES, arrayOf(
+                            "application/pdf",
+                            "application/doc",
+                            "application/docx",
+                            "text/plain"
+                        )
+                    )
+                }
+                startActivityForResult(activity,intentDocument, ImagePicker.REQUEST_CODE, Bundle.EMPTY)
+            }
+        }
+
+         fun  voiceRecord(mediaRecorder: MediaRecorder,cardVoice:CardView,activity:FragmentActivity,isRecording:Boolean){
+            var colorRose=cardVoice.getContext().getResources().getColor(R.color.light_rose);
+            var colorRed=cardVoice.getContext().getResources().getColor(R.color.red);
+            if (isRecording){
+                stopRecording(mediaRecorder)
+                cardVoice.setCardBackgroundColor(colorRose)
+            }
+            else{
+                if (checkPermissions(activity.baseContext)){
+                    startRecording(activity,mediaRecorder)
+                    cardVoice.setCardBackgroundColor(colorRed)
+                }
+                else{
+                    var arr:Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+                    ActivityCompat.requestPermissions(activity,arr,21)
+                }
+            }
+        }
+
+        private fun stopRecording(mediaRecorder: MediaRecorder) {
+            mediaRecorder.stop()
+            mediaRecorder.release()
+
+        }
+
+        private fun startRecording(activity:FragmentActivity,mediaRecorder: MediaRecorder) {
+            var recordPath:String?=activity.getExternalFilesDir("/")?.absolutePath
+            var recordFile="fileName.3gp"
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFile(recordPath+"/"+recordFile)
+            mediaRecorder.prepare()
+            mediaRecorder.start()
+        }
+
+        @RequiresApi(Build.VERSION_CODES.M)
+        fun checkPermissions(context:Context):Boolean{
+            return context?.checkSelfPermission(Manifest.permission.RECORD_AUDIO)== PackageManager.PERMISSION_GRANTED
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/lamp/ui/student/student_features_page/recitation/ReciteParagraphFragment.kt
+++ app/src/main/java/com/example/lamp/ui/student/student_features_page/recitation/ReciteParagraphFragment.kt
@@ -1,22 +1,15 @@
 package com.example.lamp.ui.student.student_features_page.recitation
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.media.MediaRecorder
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.commonFunctions.CommonFunctions
 import com.example.lamp.R
 import com.example.lamp.databinding.FragmentReciteParagraphBinding
-import com.github.dhaval2404.imagepicker.ImagePicker
 
 class ReciteParagraphFragment:Fragment() {
     lateinit var viewBinding:FragmentReciteParagraphBinding
@@ -45,76 +38,28 @@ class ReciteParagraphFragment:Fragment() {
                 .commit()
         }
         viewBinding.cardImage.setOnClickListener {
-            ImagePicker.with(this)
-                .crop()                    //Crop image(Optional), Check Customization for more option
-//                .compress(1024)			//Final image size will be less than 1 MB(Optional)
-//                .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
-                .start()
+            CommonFunctions.imagePick(this)
 
         }
 
         var isRecording=false
         viewBinding.cardVoice.setOnClickListener {
-
-            var colorRose=viewBinding.cardVoice.getContext().getResources().getColor(R.color.light_rose);
-            var colorRed=viewBinding.cardVoice.getContext().getResources().getColor(R.color.red);
             if (isRecording){
-                stopRecording()
+                CommonFunctions.voiceRecord(mediaRecorder,viewBinding.cardVoice,requireActivity(),isRecording)
                 isRecording=false
-
-                viewBinding.cardVoice.setCardBackgroundColor(colorRose)
             }
             else{
-                if (checkPermissions()){
-                    startRecording()
-                    viewBinding.cardVoice.setCardBackgroundColor(colorRed)
-                }
-                else{
-                    var arr:Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
-                    ActivityCompat.requestPermissions(requireActivity(),arr,21)
-                }
+                mediaRecorder=MediaRecorder()
+                CommonFunctions.voiceRecord(mediaRecorder,viewBinding.cardVoice,requireActivity(),isRecording)
                 isRecording=true
             }
+
         }
         viewBinding.cardDocument.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                val intentDocument = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-                    addCategory(Intent.CATEGORY_OPENABLE)
-                    type = "*/*"
-                    putExtra(
-                        Intent.EXTRA_MIME_TYPES, arrayOf(
-                            "application/pdf",
-                            "application/doc",
-                            "application/docx",
-                            "text/plain"
-                        )
-                    )
-                }
-                startActivityForResult(intentDocument, ImagePicker.REQUEST_CODE)
-            }
+            CommonFunctions.uploadDoc(requireActivity())
+
         }
 
     }
-    private fun stopRecording() {
-        mediaRecorder.stop()
-        mediaRecorder.release()
 
-    }
-
-    private fun startRecording() {
-        var recordPath:String?=requireActivity().getExternalFilesDir("/")?.absolutePath
-        var recordFile="fileName.3gp"
-        mediaRecorder= MediaRecorder()
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        mediaRecorder.setOutputFile(recordPath+"/"+recordFile)
-        mediaRecorder.prepare()
-        mediaRecorder.start()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun checkPermissions():Boolean{
-        return context?.checkSelfPermission(Manifest.permission.RECORD_AUDIO)== PackageManager.PERMISSION_GRANTED
-    }
 }
