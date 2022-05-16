deabd9615921cfebd7e44bb192de19157439690d
Pola Hano Shehat <43797542+polahano@users.noreply.github.com>
2022-05-16 23:18:39 +0200

    fragment teacher course add section

---
+++ app/src/main/java/com/example/lamp/ui/teacher/courses_page/course_content/material/TeacherCourseAddSectionFragment.kt
@@ -0,0 +1,37 @@
+package com.example.lamp.ui.teacher.courses_page.course_content.material
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.lamp.R
+import com.example.lamp.databinding.FragmentTeacherCourseAddSectionBinding
+
+class TeacherCourseAddSectionFragment : Fragment() {
+    lateinit var viewBinding: FragmentTeacherCourseAddSectionBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_teacher_course_add_section,
+            container,
+            false
+        )
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+    }
+
+    private fun initViews() {
+    }
+
+
+}
