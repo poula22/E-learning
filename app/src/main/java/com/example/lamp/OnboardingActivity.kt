503c0bc278028bd12b5b040c405d18c24ad8d147
Pola Hano Shehat <43797542+polahano@users.noreply.github.com>
2022-06-21 17:28:20 +0200

    fixing onboarding screen

--- app/src/main/java/com/example/lamp/OnboardingActivity.kt
+++ app/src/main/java/com/example/lamp/OnboardingActivity.kt
@@ -1,36 +1,61 @@
 package com.example.lamp
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.xcode.onboarding.OnBoarder
-import com.xcode.onboarding.OnBoardingPage
+import androidx.fragment.app.Fragment
+import com.aemerse.onboard.OnboardAdvanced
+import com.aemerse.onboard.OnboardFragment
 
 
-class OnBoardingActivity : AppCompatActivity() {
+class OnBoardingActivity : OnboardAdvanced() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val pages: MutableList<OnBoardingPage> = ArrayList()
-        pages.add(
-            OnBoardingPage(
-                R.drawable.student_vector,
-                "Hey, Student!",
-                "Your Platform to ease learning process and provide all Tools you need"
+        // Make sure you don't call setContentView!
+
+        // Call addSlide passing your Fragments.
+        // You can use OnboardFragment to use a pre-built fragment
+        addSlide(
+            OnboardFragment.newInstance(
+                title = "Hey, Student!",
+                description = "Your Platform to ease learning process and provide all Tools you need",
+                resourceId = R.drawable.student_vector,
+
+                titleTypefaceFontRes = R.font.robotoflex,
+                descriptionTypefaceFontRes = R.font.robotoflex,
             )
         )
-        pages.add(
-            OnBoardingPage(
-                R.drawable.teacher_vector,
-                "Hello, Teacher!",
-                "Here are all you need to help you to manage your class"
+
+        addSlide(
+            OnboardFragment.newInstance(
+                title = "Hello, Teacher!",
+                description = "Here are all you need to help you to manage your class",
+                resourceId = R.drawable.teacher_vector,
+                titleTypefaceFontRes = R.font.robotoflex,
+                descriptionTypefaceFontRes = R.font.robotoflex,
             )
         )
-        pages.add(
-            OnBoardingPage(
-                R.drawable.parent_vector,
-                "Greetings, Parent!",
-                "Follow up your children learning progress"
+
+        addSlide(
+            OnboardFragment.newInstance(
+                title = "Greetings, Parent!",
+                description = "Follow up your children learning progress",
+                resourceId = R.drawable.parent_vector,
+
+                titleTypefaceFontRes = R.font.robotoflex,
+                descriptionTypefaceFontRes = R.font.robotoflex,
             )
         )
-        OnBoarder.startOnBoarding(this, pages, null)
+    }
+
+    override fun onSkipPressed(currentFragment: Fragment?) {
+        super.onSkipPressed(currentFragment)
+        // Decide what to do when the user clicks on "Skip"
+        finish()
+    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        // Decide what to do when the user clicks on "Done"
+        finish()
     }
 }
